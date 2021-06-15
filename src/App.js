3ad4f1188b507ac924a750ef325534e807bb50e9
unnati2000 <unnatibamania8@gmail.com>
2021-06-15 21:06:48 +0530

    Login page completed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./styles/app.css";
+
+const Login = lazy(() => import("./pages/login"));
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router className="App">
+      <Suspense fallback={<p>Loading</p>}>
+        <Switch>
+          <Route path="/login" component={Login} />
+        </Switch>
+      </Suspense>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/pages/login.js
@@ -0,0 +1,84 @@
+import React, { useState, useContext, useEffect } from "react";
+import { useHistory, Link } from "react-router-dom";
+import FirebaseContext from "../context/firebase";
+
+const Login = () => {
+  const history = useHistory();
+  const { firebase } = useContext(FirebaseContext);
+
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const isInvalid = password === "" || emailAddress === "";
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    try {
+      await firebase.auth().signInWithEmailAndPassword(emailAddress, password);
+      history.push("/");
+    } catch (error) {
+      setEmailAddress("");
+      setPassword("");
+      setError(error.message);
+    }
+  };
+  return (
+    <div className="login-div">
+      <div className="login--img-div">
+        <img
+          src="https://github.com/karlhadwen/instagram/blob/master/public/images/iphone-with-profile.jpg?raw=true"
+          alt="iPhone with Instagram app"
+          height="600"
+          width="400"
+        />
+      </div>
+      <div className="login-form-div">
+        <div className="form-div">
+          <h1>Instagram</h1>
+
+          {error && <p>{error}</p>}
+
+          <form onSubmit={handleLogin} method="POST">
+            <input
+              aria-label="Enter your email address"
+              type="text"
+              placeholder="Email address"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setEmailAddress(target.value)}
+              value={emailAddress}
+            />
+            <br />
+            <input
+              aria-label="Enter your password"
+              type="password"
+              placeholder="Password"
+              className="text-sm text-gray-base w-full mr-3 py-5 px-4 h-2 border border-gray-primary rounded mb-2"
+              onChange={({ target }) => setPassword(target.value)}
+              value={password}
+            />
+            <br />
+            <button
+              disabled={isInvalid}
+              type="submit"
+              className={`${isInvalid && "buttonInvalid"}`}
+            >
+              Login
+            </button>
+          </form>
+        </div>
+        <div className="flex justify-center items-center flex-col w-full bg-white p-4 rounded border border-gray-primary">
+          <p className="text-sm">
+            Don't have an account?{` `}
+            <Link to="/signup" className="font-bold text-blue-medium">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
